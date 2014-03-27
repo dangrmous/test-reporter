c7d271ebe8f388852435dea50973995d1be04780
Geoff Unger <geoff@shopigniter.com>
2014-03-26 17:10:19 -0700

    Adjusted data fixture to deal with suites of tests

--- js/app.js
+++ js/app.js
@@ -1,150 +1,248 @@
 App = Ember.Application.create({});
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-App.Test = DS.Model.extend({
-
-    test: DS.attr(),
-   status: DS.attr(),
-    time: DS.attr(),
-    trace: DS.attr(),
-    message: DS.attr(),
-    output: DS.attr('string')
+App.Suite = DS.Model.extend({
+    date: DS.attr(),
+    tests: DS.attr()
 });
 
-App.Test.FIXTURES = [
-    {"id": 1,
-        "test": "FBLinkPostTest::testFBLinkPost",
-        "status": "error",
-        "time": 52.387059926987,
-        "trace": [
+App.Suite.FIXTURES = [
+    {
+        "id": 1,
+        "date": 032520141103,
+        "suite": [
+            {"test": "CreateCampaignTest::testCreateCampaign",
+                "status": "fail",
+                "time": 19.10386300087,
+                "trace": [
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
+                        "line": 12,
+                        "function": "__call",
+                        "class": "PHPUnit_Extensions_SeleniumTestCase",
+                        "type": "->",
+                        "args": [
+                            "assertTextPresent",
+                            [
+                                "sdfasj"
+                            ]
+                        ]
+                    },
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
+                        "line": 12,
+                        "function": "assertTextPresent",
+                        "class": "CreateCampaignTest",
+                        "type": "->",
+                        "args": [
+                            "sdfasj"
+                        ]
+                    }
+                ],
+                "message": "Failed command: assertTextPresent('sdfasj')\nFailed asserting that false is true.",
+                "output": ""},
             {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
-                "line": 13,
-                "function": "__call",
-                "class": "PHPUnit_Extensions_SeleniumTestCase",
-                "type": "->",
-                "args": [
-                    "click",
-                    [
-                        "3ni478faiucgnfc"
-                    ]
-                ]
+                "test": "FBLinkPostTest::testFBLinkPost",
+                "status": "fail",
+                "time": 76.359910011292,
+                "trace": [
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
+                        "line": 13,
+                        "function": "__call",
+                        "class": "PHPUnit_Extensions_SeleniumTestCase",
+                        "type": "->",
+                        "args": [
+                            "waitForElementPresent",
+                            [
+                                "css=#sdaf"
+                            ]
+                        ]
+                    },
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
+                        "line": 13,
+                        "function": "waitForElementPresent",
+                        "class": "FBLinkPostTest",
+                        "type": "->",
+                        "args": [
+                            "css=#sdaf"
+                        ]
+                    }
+                ],
+                "message": "WaitFor timeout. \nLast exception message: \nFailed command: waitForElementPresent('css=#sdaf')\nFailed asserting that false is true.",
+                "output": ""
             },
             {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
-                "line": 13,
-                "function": "click",
-                "class": "FBLinkPostTest",
-                "type": "->",
-                "args": [
-                    "3ni478faiucgnfc"
-                ]
-            }
-        ],
-        "message": "\nInvalid response while accessing the Selenium Server at 'http:\/\/localhost:4444\/selenium-server\/driver\/': ERROR: Element 3ni478faiucgnfc not found",
-        "output": ""
+                "test": "FBUnpublishedLinkPostTest::testFBUnpublishedLinkPost",
+                "status": "pass",
+                "time": 51.292876958847,
+                "trace": [
 
-    },
-    {"id": 2,
-        "test": "CreateCampaignTest::testCreateCampaign",
-        "status": "fail",
-        "time": 25.21940779686,
-        "trace": [
-            {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
-                "line": 12,
-                "function": "__call",
-                "class": "PHPUnit_Extensions_SeleniumTestCase",
-                "type": "->",
-                "args": [
-                    "assertTextPresent",
-                    [
-                        "sdfasj"
-                    ]
-                ]
+                ],
+                "message": "",
+                "output": ""
             },
             {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
-                "line": 12,
-                "function": "assertTextPresent",
-                "class": "CreateCampaignTest",
-                "type": "->",
-                "args": [
-                    "sdfasj"
-                ]
-            }
-        ],
-        "message": "Failed command: assertTextPresent('sdfasj')\nFailed asserting that false is true.",
-        "output": ""
-    },
-    {"id": 3,
-        "test": "CreateCampaignTest::testCreateCampaign",
-        "status": "fail",
-        "time": 21.426814079285,
-        "trace": [
-            {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
-                "line": 12,
-                "function": "__call",
-                "class": "PHPUnit_Extensions_SeleniumTestCase",
-                "type": "->",
-                "args": [
-                    "assertTextPresent",
-                    [
-                        "sdfasj"
-                    ]
-                ]
-            },
-            {
-                "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
-                "line": 12,
-                "function": "assertTextPresent",
-                "class": "CreateCampaignTest",
-                "type": "->",
-                "args": [
-                    "sdfasj"
-                ]
-            }
-        ],
-        "message": "Failed command: assertTextPresent('sdfasj')\nFailed asserting that false is true.",
-        "output": ""
-    },
-    {"id": 4,
-        "test": "CreateCampaignTest::testCreateCampaign",
-        "status": "pass",
-        "time": 28.407824993134,
-        "trace": [
+                "test": "FBFutureLinkPostTest::testFBFutureLinkPost",
+                "status": "pass",
+                "time": 44.546409130096,
+                "trace": [
 
-        ],
-        "message": "",
-        "output": ""
+                ],
+                "message": "",
+                "output": ""
+            },
+            {
+                "test": "CreateFBAdTest::testCreateFBAd",
+                "status": "pass",
+                "time": 27.856699943542,
+                "trace": [
+
+                ],
+                "message": "",
+                "output": ""
+            }
+        ]},
+    {
+        "id": 2,
+        "date": 032520141116,
+        "tests": [
+            {
+                "test": "CreateCampaignTest::testCreateCampaign",
+                "status": "fail",
+                "time": 21.426814079285,
+                "trace": [
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
+                        "line": 12,
+                        "function": "__call",
+                        "class": "PHPUnit_Extensions_SeleniumTestCase",
+                        "type": "->",
+                        "args": [
+                            "assertTextPresent",
+                            [
+                                "sdfasj"
+                            ]
+                        ]
+                    },
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/CreateCampaignTest.php",
+                        "line": 12,
+                        "function": "assertTextPresent",
+                        "class": "CreateCampaignTest",
+                        "type": "->",
+                        "args": [
+                            "sdfasj"
+                        ]
+                    }
+                ],
+                "message": "Failed command: assertTextPresent('sdfasj')\nFailed asserting that false is true.",
+                "output": ""
+            },
+            {
+                "test": "FBLinkPostTest::testFBLinkPost",
+                "status": "fail",
+                "time": 69.252578020096,
+                "trace": [
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
+                        "line": 13,
+                        "function": "__call",
+                        "class": "PHPUnit_Extensions_SeleniumTestCase",
+                        "type": "->",
+                        "args": [
+                            "waitForElementPresent",
+                            [
+                                "css=#sdaf"
+                            ]
+                        ]
+                    },
+                    {
+                        "file": "\/Library\/WebServer\/Documents\/test-automation\/SmokeTests\/FBLinkPostTest.php",
+                        "line": 13,
+                        "function": "waitForElementPresent",
+                        "class": "FBLinkPostTest",
+                        "type": "->",
+                        "args": [
+                            "css=#sdaf"
+                        ]
+                    }
+                ],
+                "message": "WaitFor timeout. \nLast exception message: \nFailed command: waitForElementPresent('css=#sdaf')\nFailed asserting that false is true.",
+                "output": ""
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": 032520141523,
+        "tests": [
+            {
+                "test": "CreateCampaignTest::testCreateCampaign",
+                    "status": "pass",
+                    "time": 28.407824993134,
+                    "trace": [
+
+                    ],
+                    "message": "",
+                    "output": ""
+                },
+            {
+                    "test": "FBLinkPostTest::testFBLinkPost",
+                    "status": "pass",
+                    "time": 48.374269962311,
+                    "trace": [
+
+                    ],
+                    "message": "",
+                    "output": ""
+            },
+            {
+                "test": "FBUnpublishedLinkPostTest::testFBUnpublishedLinkPost",
+                    "status": "pass",
+                    "time": 49.095010042191,
+                    "trace": [
+
+                    ],
+                    "message": "",
+                    "output": ""
+            },
+            {
+                "test": "FBFutureLinkPostTest::testFBFutureLinkPost",
+                    "status": "pass",
+                    "time": 47.126768112183,
+                    "trace": [
+
+                    ],
+                    "message": "",
+                    "output": ""
+            }
+        ]
     }
 ]
-
-
-
 
 
 App.Router.map(function () {
 
     this.resource('about');
-    this.resource('tests', function () {
-        this.resource('test', {path: ':test_id'});
+    this.resource('suites', function () {
+        this.resource('suite', {path: ':suite_id'});
     });
     // put your routes here
 });
 
-App.TestsRoute = Ember.Route.extend({
+App.SuitesRoute = Ember.Route.extend({
     model: function () {
-        return this.store.findAll('test');
+        return this.store.findAll('suite');
     }
 
 });
 
-App.TestRoute = Ember.Route.extend({
+App.SuiteRoute = Ember.Route.extend({
     model: function (params) {
 
-        return this.store.find('test', params.test_id);
+        return this.store.find('suite', params.suite_id);
     }
 });
 
